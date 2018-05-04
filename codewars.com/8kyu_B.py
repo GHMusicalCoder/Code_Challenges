f55a6b11c3e760eb7700eaf7c7afdd346039483e
GHMusicalCoder <github@musicalcoder.com>
2018-05-04 16:58:23 -0400

    adding B file

---
+++ codewars.com/8kyu_B.py
@@ -0,0 +1,37 @@
+# B01 Is this my tail? - https://www.codewars.com/kata/is-this-my-tail
+def correct_tail(body, tail):
+    return tail == body[-1]
+
+
+def correct_tail_bp(body, tail):
+    return body.endswith(tail)
+
+
+# B02 Calculate average - https://www.codewars.com/kata/calculate-average
+def find_average(array):
+    return sum(array)/len(array) if (sum(array) != 0) else 0
+
+
+def find_average_bp(array):
+    return sum(array) / len(array) if array else 0
+
+
+# B03 Jenny's secret message - https://www.codewars.com/kata/jennys-secret-message
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)
+
+
+# B04 Parse nice int from char problem - https://www.codewars.com/kata/parse-nice-int-from-char-problem
+def get_age(age):
+    return int(age[0])
+
+
+# B05 String cleaning - https://www.codewars.com/kata/string-cleaning
+def string_clean(s):
+    return ''.join([c for c in s if c not in '1234567890'])
+
+
+def string_clean_bp(s):
+    return ''.join(x for x in s if not x.isdigit())
