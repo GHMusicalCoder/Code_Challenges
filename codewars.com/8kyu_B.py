d635efa190935f31e79fff84db6e3e0f416505ee
GHMusicalCoder <github@musicalcoder.com>
2018-05-07 09:50:01 -0400

    adding 5 more prior katas to B

--- codewars.com/8kyu_B.py
+++ codewars.com/8kyu_B.py
@@ -35,3 +35,34 @@ def string_clean(s):
 
 def string_clean_bp(s):
     return ''.join(x for x in s if not x.isdigit())
+
+
+# B06 String repeat - https://www.codewars.com/kata/string-repeat
+def repeat_str(repeat, string):
+    return string * repeat
+
+
+# B07 Hex to Decimal - https://www.codewars.com/kata/hex-to-decimal
+def hex_to_dec(s):
+    return int(s, 16)
+
+
+# B08 Bin to Decimal - https://www.codewars.com/kata/bin-to-decimal
+def bin_to_decimal(inp):
+    return int(inp, 2)
+
+
+# B09 Will you make it? -  https://www.codewars.com/kata/will-you-make-it
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    return True if (mpg * fuel_left) >= distance_to_pump else False
+
+
+def zeroFuel_bp(distance_to_pump, mpg, fuel_left):
+    return distance_to_pump <= mpg * fuel_left
+
+
+#B10 Convert boolean values to strings 'Yes' or 'No'. -
+# https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no
+def bool_to_word(boolean):
+    return 'Yes' if boolean else 'No'
+
